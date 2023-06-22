f7763c35af2b7f10b1031568239051df71628de2
asadcs <muhammad.asad2020@gmail.com>
2023-06-21 23:22:25 -0700

    Discount Service Repository pattern developed

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,8 +1,13 @@
+using Discount.API.Repositories.Interfaces;
+using Discount.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
-
+var Configuration = builder.Configuration;
 // Add services to the container.
-
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 builder.Services.AddControllers();
+//builder.Services.AddHealthChecks()
+//              .AddNpgSql(Configuration["DatabaseSettings:ConnectionString"]);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
