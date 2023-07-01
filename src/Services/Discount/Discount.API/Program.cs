e035decafb100db588f8c870f699e997415bb795
asadcs <muhammad.asad2020@gmail.com>
2023-06-30 22:46:15 -0700

    Discount GRPC Server Completed Development

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,11 +1,19 @@
 using Discount.API.Repositories.Interfaces;
 using Discount.API.Repositories;
+using Microsoft.AspNetCore.Hosting;
+using Discount.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 var Configuration = builder.Configuration;
 // Add services to the container.
 builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 builder.Services.AddControllers();
+
+//var host = CreateHostBuilder(args).Build();
+//host.MigrateDatabase<Program>();
+//host.Run();
+
+
 //builder.Services.AddHealthChecks()
 //              .AddNpgSql(Configuration["DatabaseSettings:ConnectionString"]);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -26,3 +34,28 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+partial class Program
+{
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.ConfigureServices((hostContext, services) =>
+                {
+                    // Configure your services here
+                });
+
+                webBuilder.Configure((hostContext, app) =>
+                {
+                    // Configure your app's pipeline here
+                    app.UseRouting();
+
+                    app.UseEndpoints(endpoints =>
+                    {
+                        // Configure your endpoints here
+                    });
+                });
+            });
+
+}
